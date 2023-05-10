0282a191650068037be6b58661aa8c2c542b4525
NauSakaguchi <sakaguchinau@gmail.com>
2023-05-09 17:01:36 -0700

    impl SIR simulation

--- sir.py
+++ sir.py
@@ -1,8 +1,14 @@
+import shutil
+
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
 import random
+import os
+import numpy as np
+
+output_dir = './result'
 
 
 def create_graph_from(filename):
@@ -39,35 +45,49 @@ def create_graph_from(filename):
 
 
 def draw_result(cnt_list, title):
-    print("Number of infected nodes: {}".format(cnt_list))
-    # グラフの描画
+    path = output_dir + '/' + title + '.png'
+    # print("Number of infected nodes: {}".format(cnt_list))
+
     fig, ax = plt.subplots()
-    x = list(range(len(cnt_list)))  # x軸の座標を作成
+    x = list(range(len(cnt_list)))  # create x axis
     ax.bar(x, cnt_list, width=1)
-    # 軸ラベルとタイトルの設定
+    # set x axis label and y axis label
     ax.set_xlabel('hours')
     ax.set_ylabel('People')
     ax.set_title(title)
-    # グラフの表示
-    plt.show()
+    plt.gca().margins(x=0)
+
+    # save graph
+    plt.savefig(path)
+    # plt.show() # show graph
+
+    plt.close()
 
 
+####################
+# SIR model ########
+# G: graph
+# initial_infected_nodes: the list of initial infected nodes
+# init_cnt: the number of initial infected nodes
+# steps: the number of steps
+# infection_rate: the probability of infection
+# bias: the bias of infection rate
+# return: the list of the number of infected nodes
 def sir_simulation(G, initial_infected_nodes, init_cnt, steps, infection_rate=0.01, bias=0.00005):
-    # すべてのノードをSusceptibleに設定
-    # infection_rate = 0.01
     cnt = init_cnt
     cnt_list = []
+    # set all nodes to susceptible
     status = {node: 'S' for node in G.nodes()}
     for node in initial_infected_nodes:
         status[node] = 'I'
     for t in range(steps):
         # show step number
-        print("Step: {}".format(t + 1))
-        # タイムステップごとの感染と回復のプロセス
+        # print("Step: {}".format(t + 1))
+        # process of infection for each time step
         new_status = status.copy()
         for node in G.nodes():
             if status[node] == 'I':
-                # 感染プロセス
+                # infection process
                 neighbors = list(G.successors(node))
                 for neighbor in neighbors:
                     if status[neighbor] == 'S' and random.random() < infection_rate:
@@ -77,7 +97,7 @@ def sir_simulation(G, initial_infected_nodes, init_cnt, steps, infection_rate=0.
         status = new_status
         infection_rate = infection_rate - bias
         # print cnt
-        print("Number of infected nodes: {}".format(cnt))
+        # print("Number of infected nodes: {}".format(cnt))
     return cnt_list
 
 
@@ -92,9 +112,11 @@ def get_communities_from(G):
 
 
 def reduce_graph_size(G):
+    # k-core decomposition
     k_core = 20
     G = nx.k_core(G, k=k_core)
 
+    # remove nodes with low in-degree
     min_in_degree = 20
     filtered_nodes = [node for node, in_degree in G.in_degree() if in_degree >= min_in_degree]
     G = G.subgraph(filtered_nodes)
@@ -107,48 +129,122 @@ def get_top_pr_from(communities, pr, list_size):
     pr_sorted = sorted(pr.items(), key=lambda x: x[1], reverse=True)
 
     top_list = []
-    # show top 1 node in each community
-    print("Top 1 node in each community by PageRank")
+    # get top 1 node in each community
     for i in range(list_size):
         community = communities[i % len(communities)]
         pr_community = {node: pr[node] for node in community}
         pr_community_sorted = sorted(pr_community.items(), key=lambda x: x[1], reverse=True)
-        print("Community: {}, Node: {}, PageRank: {}".format(community, pr_community_sorted[0][0],
-                                                             pr_community_sorted[0][1]))
+        # print("Community: {}, Node: {}, PageRank: {}".format(community, pr_community_sorted[0][0],
+        #                                                      pr_community_sorted[0][1]))
         top_list.append(pr_community_sorted[0][0])
     return top_list
 
 
 # main function
 def main():
+    ###############################
+    # create a directory to save the result
+    ###############################
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    else:
+        shutil.rmtree(output_dir)
+        os.mkdir(output_dir)
+    ###########################
+
+    ###############################
+    # create a graph from the dataset
+    ###############################
     G, id_index_dict = create_graph_from("data.csv")
 
-    # 情報伝搬の方向はフォロー関係とは逆になる。
-    # つまり、フォローされている人の情報がフォロワーに伝わる
+    # the direction of information propagation is opposite to the follow relationship.
+    # In other words, the information of the person who is followed is propagated to the follower.
     G = G.reverse()
+    ###########################
 
+    ###############################
     # set parameters for SIR simulation
-    init_cnt = 1
+    ###############################
     hours = 200
+    trial_cnt = 1
+    first_s_result = []
+    second_s_result = []
+    # (start, end, step)
+    experiment_range = (1, 5, 2)
+    ###########################
+    for init_cnt in range(experiment_range[0], experiment_range[1], experiment_range[2]):
+        print("{}/{}".format(init_cnt, experiment_range[1]))
+        result = first_strategy(G, hours, init_cnt, trial_cnt)
+        first_s_result.append(result)
 
+        result = second_strategy(G, hours, init_cnt)
+        second_s_result.append(result)
+
+    # create x axis index
+    index = np.arange(experiment_range[0], experiment_range[1], experiment_range[2])
+
+    bar_width = 1
+    plt.bar(index, first_s_result, width=bar_width, color='blue', label='First Strategy')
+    plt.bar(index + bar_width, second_s_result, width=bar_width, color='orange', label='Second Strategy')
+
+    # set x axis label
+    plt.xlabel('Initial Infected Count')
+    # plt.xticks(index + bar_width / 2, range(1, 11, 2))
+
+    # set y axis label
+    plt.ylabel('Results')
+
+    # set title
+    plt.title('Comparison of First Strategy and Second Strategy')
+    plt.legend()
+
+    plt.gca().margins(x=0)
+
+    plt.show()
+
+
+def first_strategy(G, hours, init_cnt, trial_cnt):
+    ###############################
     # First Strategy
-    # setup
-    # ランダムにいくつかのノードをInfectedに設定
-    initial_infected_nodes = random.sample(list(G.nodes), init_cnt)  # 5は初期感染ノードの数を表します
+    ###############################
+    max_cnt = 0
+    best_result = []
+    total_cnt = 0
+    for i in range(trial_cnt):
+        print("Trial: {}".format(i + 1))
+        # setup
+        # ランダムにいくつかのノードをInfectedに設定
+        initial_infected_nodes = random.sample(list(G.nodes), init_cnt)  # 5は初期感染ノードの数を表します
 
-    # execute SIR simulation
-    cnt_list = sir_simulation(G, initial_infected_nodes, init_cnt, hours)
-    draw_result(cnt_list, "Random")
+        # execute SIR simulation
+        cnt_list = sir_simulation(G, initial_infected_nodes, init_cnt, hours)
+        total_cnt += cnt_list[-1]
+        if cnt_list[-1] > max_cnt:
+            max_cnt = cnt_list[-1]
+            best_result = cnt_list
+    # show the average number of infected nodes
+    print("Average number of infected nodes: {}".format(total_cnt / trial_cnt))
+    draw_result(best_result, str(init_cnt).zfill(3) + "-Random")
 
+    return total_cnt / trial_cnt
+    ###########################
+
+
+def second_strategy(G, hours, init_cnt):
+    ###############################
     # Second Strategy
+    ###############################
     # setup
     preprocessed_graph = reduce_graph_size(G)
-    initial_infected_nodes = get_top_pr_from(get_communities_from(preprocessed_graph),  nx.pagerank(preprocessed_graph),
+    initial_infected_nodes = get_top_pr_from(get_communities_from(preprocessed_graph), nx.pagerank(preprocessed_graph),
                                              init_cnt)
-
     # execute SIR simulation
     cnt_list = sir_simulation(G, initial_infected_nodes, init_cnt, hours)
-    draw_result(cnt_list, "PageRank")
+    print("Infected nodes: {}".format(cnt_list[-1]))
+    draw_result(cnt_list, str(init_cnt).zfill(3) + "-PageRank")
+
+    return cnt_list[-1]
+    ###########################
 
 
 if __name__ == "__main__":
